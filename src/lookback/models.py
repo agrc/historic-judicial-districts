d4ee7466df4cedf8ba0f0ea9ae5ed75bfc6b54d9
Jake Adams <jdadams@utah.gov>
2021-03-10 16:54:15 -0700

    feat: combine dates

--- src/lookback/models.py
+++ src/lookback/models.py
@@ -4,6 +4,13 @@ import arcpy
 import pandas as pd
 
 
+def clean_name(name):
+    clean_name = name.casefold()
+    clean_name = clean_name.replace(' ', '')
+    clean_name = clean_name.replace('.', '')
+    return clean_name
+
+
 class ChangeDate:
 
     def __init__(self, date):
@@ -18,17 +25,18 @@ class ChangeDate:
 class State:
 
     def __init__(self):
-        counties_df = None
-        districts_df = None
-        counties = []  #: list of County objects
+        self.counties_df = None
+        self.districts_df = None
+        self.counties = []  #: list of County objects
 
     def load_counties(self, counties_shp):
 
         #: Read counties shapefile in as dict, transform dict to dataframe
+        print(f'Loading counties from {counties_shp}...')
         counties_list = []
         county_fields = [f.name for f in arcpy.ListFields(counties_shp)]
-        with arcpy.da.SearchCursor(counties_shp, county_fields) as sc:
-            for row in sc:
+        with arcpy.da.SearchCursor(counties_shp, county_fields) as search_cursor:
+            for row in search_cursor:
                 counties_list.append(dict(zip(county_fields, row)))
         self.counties_df = pd.DataFrame(counties_list)
 
@@ -37,7 +45,11 @@ class State:
 
     def load_districts(self, districts_csv):
 
+        print(f'Loading districts from {districts_csv}...')
         self.districts_df = pd.read_csv(districts_csv)
+
+        #: change name to better match the county shapefile id
+        self.districts_df['CountyName'] = self.districts_df['CountyName'].apply(clean_name)
 
         #: Clean up dates
         self.districts_df['StartDate'] = pd.to_datetime(self.districts_df['StartDate'])
@@ -53,18 +65,30 @@ class State:
 
         county_names = self.districts_df['CountyName'].unique()
         for name in county_names:
+            print(f'Setting up {name}...')
             county = County(name)
             county.setup(self.counties_df, self.districts_df)
             self.counties.append(county)
+
+    def calc_counties(self):
+        for county in self.counties:
+            county.calc_change_dates()
+
+    def combine_change_dfs(self, out_path):
+        combined_change_df = pd.DataFrame()  #columns=['date', 'county_version', 'district'])
+        for county in self.counties:
+            combined_change_df = combined_change_df.append(county.change_dates_df)
+        # print(combined_change_df)
+        combined_change_df.to_csv(out_path)
 
 
 class County:
 
     def __init__(self, name):
-        self.name = name.casefold()
+        self.name = name
         self.shape_df = None  #: All entries for this county in the historical county shape/geography dataframe
-        self.district_df = None  #: All entries for this county in the historical dsitrict composition dataframe
-        self.all_dates = None  #: Series of start dates day from both shape_df and district_df
+        self.district_df = None  #: All entries for this county in the historical district composition dataframe
+        self.change_dates_df = None  #: The change dates with corresponding district and shape versions
 
     def setup(self, counties_df, districts_df):
         #: create a local shape dataframe of just the county, change the index to the start date of each version
@@ -77,10 +101,31 @@ class County:
         self.district_df.set_index('StartDate', inplace=True)
         self.district_df.sort_index(inplace=True)
 
-        #: create series of dates to check
-        self.all_dates = pd.Series(self.shape_df.index.union(self.district_df.index))
-
     def get_versions(self):
         #         check_dates = self.all_dates + pd.Timedelta(days=1)
         #         versions_df = pd.DataFrame(self.all_dates + pd.Timedelta(days=1))
         pass
+
+    def calc_change_dates(self):
+
+        #: Get a list of ChangeDate objects for all the dates for this county
+        dates = list(pd.Series(self.shape_df.index.union(self.district_df.index)))
+        change_dates = [ChangeDate(date) for date in dates]
+
+        for change_date in change_dates:
+            #: Get the county key for this date
+            for county_row in self.shape_df.itertuples():
+                if change_date.date >= county_row.Index and change_date.date <= county_row.END_DATE:
+                    change_date.county_version = county_row.county_key
+                    break
+            #: Get the district key for this date
+            for district_row in self.district_df.itertuples():
+                if change_date.date >= district_row.Index and change_date.date <= district_row.EndDate:
+                    change_date.district = district_row.NewDistrict
+                    break
+
+        #: Now we can use a namedtuple to translate our object to dataframe rows
+        #: (Didn't use one at first because we had to alter the list of ChangeDate items in the iterations)
+        ChangeDatesTuple = namedtuple('ChangeDatesTuple', 'date county_version district')
+        new_dates = [ChangeDatesTuple(cd.date, cd.county_version, cd.district) for cd in change_dates]
+        self.change_dates_df = pd.DataFrame(new_dates)
