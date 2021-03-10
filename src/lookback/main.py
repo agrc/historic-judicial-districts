e82613d7f96d649daa1869f8e08367c402a31762
Jake Adams <jdadams@utah.gov>
2021-03-10 16:54:15 -0700

    feat: combine dates

--- src/lookback/main.py
+++ src/lookback/main.py
@@ -5,16 +5,24 @@ a description of what this module does.
 this file is for testing linting...
 """
 
-from . import models
+from lookback.models import State
 
-if __name__ == '__main__':
 
+def main():
     districts_csv = r'C:\gis\Projects\HistoricCounties\Data\JudicialDistricts\test.csv'
     counties_shp = r'C:\gis\Projects\HistoricCounties\Data\HistoricalCountyBoundaries\UT_Historical_Counties\UT_Historical_Counties.shp'
+    change_dates_csv = r'C:\gis\Projects\HistoricCounties\Data\JudicialDistricts\change_dates.csv'
 
-    state = models.State()
+    state = State()
     state.load_counties(counties_shp)
     state.load_districts(districts_csv)
     state.setup_counties()
+    state.calc_counties()
 
-    print(state.counties['Beaver'])
+    # print(state.counties[1].change_dates_df)
+    state.combine_change_dfs(change_dates_csv)
+
+
+if __name__ == '__main__':
+
+    main()
