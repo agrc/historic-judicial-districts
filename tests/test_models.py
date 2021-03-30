ca8bdb12bad0897c9d3cf7e64533bbbb3ee5d026
Jake Adams <jdadams@utah.gov>
2021-03-30 15:44:30 -0600

    fix: update existing test

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,43 +1,53 @@
+import numpy as np
 import pandas as pd
+import pytest
 
 from lookback import models
 
 
-def test_calc_change_dates(mocker):
+@pytest.fixture
+def shape_data():
+    shape_df = pd.DataFrame(
+        data={
+            'county_key': ['uts_co_S1', 'uts_co_s2', 'uts_co_s3', 'uts_co_s4'],
+            'START_DATE': ['2020-02-05', '2020-02-20', '2020-02-25', '2020-03-01'],
+            'END_DATE': ['2020-02-19', '2020-02-24', '2021-02-28', '2021-01-01'],
+        },
+    )
+    shape_df['END_DATE'] = pd.to_datetime(shape_df['END_DATE'])
+    shape_df['START_DATE'] = pd.to_datetime(shape_df['START_DATE'])
+    shape_df.set_index('START_DATE', drop=False, inplace=True)
+
+    return shape_df
+
+
+@pytest.fixture
+def district_data():
+    district_df = pd.DataFrame(
+        data={
+            'NewDistrict': ['1', '2', '3', '4'],
+            'district_key': ['co_D1', 'co_D2', 'co_D3', 'co_D4'],
+            'StartDate': ['2020-02-10', '2020-02-15', '2020-02-25', '2020-03-01'],
+            'EndDate': ['2020-02-14', '2020-02-24', '2020-02-28', None],
+        },
+    )
+    district_df['StartDate'] = pd.to_datetime(district_df['StartDate'])
+    district_df['EndDate'] = pd.to_datetime(district_df['EndDate'])
+    district_df.set_index('StartDate', drop=False, inplace=True)
+
+    return district_df
+
+
+def test_change_dates_without_district_end_date(mocker, shape_data, district_data):
     county = mocker.Mock()
+    county.name = 'co'
+    county.shape_df = shape_data
+    county.district_df = district_data
 
-    #: Mock up shape and districts df
-    county.shape_df = pd.DataFrame(
-        data={
-            'county_key': ['uts_co_S1', 'uts_co_s2', 'uts_co_s3'],
-            'END_DATE': ['2020-02-19', '2020-02-24', '2020-02-28'],
-        },
-        index=['2020-02-05', '2020-02-20', '2020-02-25']
-    )
-    county.shape_df.index = pd.to_datetime(county.shape_df.index)
-    county.shape_df['END_DATE'] = pd.to_datetime(county.shape_df['END_DATE'])
-
-    county.district_df = pd.DataFrame(
-        data={
-            'NewDistrict': ['1', '2', '3'],
-            'EndDate': ['2020-02-14', '2020-02-24', '2020-02-28'],
-        },
-        index=['2020-02-10', '2020-02-15', '2020-02-25']
-    )
-    county.district_df.index = pd.to_datetime(county.district_df.index)
-    county.district_df['EndDate'] = pd.to_datetime(county.district_df['EndDate'])
-
-    #: Actual Test
     models.County.calc_change_dates(county)
 
-    #: Data output dataframe should match this
-    test_df = pd.DataFrame(
-        data={
-            'date': ['2020-02-05', '2020-02-10', '2020-02-15', '2020-02-20', '2020-02-25'],
-            'county_version': ['uts_co_S1', 'uts_co_S1', 'uts_co_S1', 'uts_co_s2', 'uts_co_s3'],
-            'district': ['n/a', '1', '2', '2', '3'],
-        }
-    )
-    test_df['date'] = pd.to_datetime(test_df['date'])
+    test_columns = ['date', 'county_name', 'county_version', 'district_number', 'district_version']
 
-    assert test_df.equals(county.change_dates_df)
+    assert county.change_dates_df.loc[5, test_columns].values.tolist() == [
+        np.datetime64('2020-03-01'), 'co', 'uts_co_s4', '4', 'co_D4'
+    ]
